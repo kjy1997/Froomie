48f97bc81733e2dd87930d9317421b4c8b6ef8a9
Danny Zou <zou50@purdue.edu>
2017-09-28 15:25:38 -0400

    Create separate profile for users without a palce

--- imports/App.jsx
+++ imports/App.jsx
@@ -5,6 +5,7 @@ import { render } from 'react-dom';
 import ReactDOM from 'react-dom';
 import SignUpWithPlace from './SignUpWithPlace.jsx';
 import UserProfile from './UserProfile.jsx';
+import UserProfileNoPlace from './UserProfileNoPlace.jsx';
 
 export default class App extends Component {
 
@@ -15,6 +16,7 @@ export default class App extends Component {
         <Route path="/home" component={Home}/>
         <Route path="/signupwithplace" component={SignUpWithPlace}/>
         <Route path="/profile" component={UserProfile}/>
+        <Route path="/profilenoplace" component={UserProfileNoPlace}/>
         </div>
        </Router>
     );
